278792785bd8ff70e30d9ebd397883a4708f9b45
Munees007 <muneesm458@gmail.com>
2024-09-11 08:25:27 +0530

    thank you page

--- src/Pages/ThankYou.tsx
+++ src/Pages/ThankYou.tsx
@@ -1,10 +1,14 @@
+import "../Modules/themes"
 const ThankYou = ()=>{
     const getTheme = () =>{
         return localStorage.getItem("theme") || "dracula";
     }
     return(
-        <div>
+        <div className={`flex-col w-full h-screen ace-${getTheme} flex justify-center items-center`}>
+            <p className="text-[8rem] uppercase font-serif">Thank You</p>
+        <div className="ace-dracula w-full h-screen">
             <p>Thank You</p>
+        </div>
         </div>
     )
 }
