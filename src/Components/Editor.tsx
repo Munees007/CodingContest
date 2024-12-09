d60d19b0889895b71af25c42b42bf009694ced33
muneesm458 <muneesm458@gmail.com>
2024-12-09 11:17:20 +0000

    bug in language local storage is fixed

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -262,7 +262,7 @@ const Editor: React.FC<EditorProps> = ({useLevel,levelIndex, ExecuteCode, Result
       
       questions.finalAnswer[levelNo].answer[i-1] = {
         code: localStorage.getItem("Level" + getCurrentLevelIndex() + keyPrefix) || "",
-        language: localStorage.getItem("Level" + getCurrentLevelIndex() +`${keyPrefix}language`) || "java",
+        language: localStorage.getItem("Level" + getCurrentLevelIndex() + "Question" +`${keyPrefix}language`) || "java",
         output: localStorage.getItem("Level" + getCurrentLevelIndex() +"Question"+`${keyPrefix}output`) || "",
         answered:Boolean(localStorage.getItem("Level" + getCurrentLevelIndex() +"question" + i + "answered")) || false
       };
@@ -380,6 +380,7 @@ const Editor: React.FC<EditorProps> = ({useLevel,levelIndex, ExecuteCode, Result
     }
   return (
     <div className={`ace-${theme ? theme : "dracula"} ${useLevel ? "pointer-events-none" :""} ${codeData?.finalAnswer[currentLevelIndex]?.answer[questionNo-1]?.answered ? "pointer-events-none" : ""} relative h-screen p-5 overflow-hidden`}>
+      <button onClick={()=>{console.log()}}>click</button>
       <p className="text-4xl font-bold text-center">CODING CONTEST</p>
       {
         codeData?.finalAnswer[currentLevelIndex]?.answer[questionNo-1]?.answered && <p className="text-xl text-green-500 uppercase font-bold absolute top-5 right-10">completed</p>
